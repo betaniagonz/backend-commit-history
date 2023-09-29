6f3fa203fcf71ace665d82720ee9443131c494f1
Betania Gonzalez <betaniagonz@gmail.com>
2023-09-29 04:28:05 -0500

    add options in SwaggerModule.setup method

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,14 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('doc', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
   app.enableCors();
   await app.listen(3000);
 }
